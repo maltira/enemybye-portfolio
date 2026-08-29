0eb3c93e143795ccb0aedbe59b1b5cc48e8bdd6a
maltira <andrejpanov382@gmail.com>
2026-08-29 14:28:27 +0300

    chore(config): настроить vite с scss, единый tsconfig и алиасы путей

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/app/styles/variables.scss" as *;\n@use "@/app/styles/mixins.scss" as *;\n`,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
 })
+
